27dbb560eea23c6637f04cc3ed8aab2334f231c9
unknown <rizwanansari84055@gmail.com>
2024-09-22 07:07:18 -0700

    Chnge into vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,49 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import autoprefixer from 'autoprefixer'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    base: './',
-    build: {
-      outDir: 'build',
-    },
-    css: {
-      postcss: {
-        plugins: [
-          autoprefixer({}), // add options if needed
-        ],
-      },
-    },
-    esbuild: {
-      loader: 'jsx',
-      include: /src\/.*\.jsx?$/,
-      exclude: [],
-    },
-    optimizeDeps: {
-      force: true,
-      esbuildOptions: {
-        loader: {
-          '.js': 'jsx',
-        },
-      },
-    },
-    plugins: [react()],
-    resolve: {
-      alias: [
-        {
-          find: 'src/',
-          replacement: `${path.resolve(__dirname, 'src')}/`,
-        },
-      ],
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
-    },
-    server: {
-      port: 3000,
-      proxy: {
-        // https://vitejs.dev/config/server-options.html
-      },
-    },
-  }
-})
+export default defineConfig({
+  base: '/Phishing-URL-Detection/',  // Set this to your GitHub repository name
+  build: {
+    outDir: 'dist',  // This specifies the output directory for the build
+  },
+  plugins: [react()],  // Enable React support
+});
